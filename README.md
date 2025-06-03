10e60e85b4243f00f116d767568a9c350eec9511
jeonggul <dlwjdgkw@gmail.com>
2025-06-03 21:37:37 +0900

    로그인/회원가입/검색결과창

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# stage_front
+# stage_play
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import login from '@/views/login.vue'
+import signup from '@/views/signup.vue'
+import signup_details from '@/views/signup_details.vue'
+import search_result from '@/views/search_result.vue'
+
+const routes = [
+    {path:'/', redirect: '/login'},
+    {path:'/login', component: login},
+    {path: '/signup', component: signup},
+    {path: '/signup_details', component: signup_details},
+    {path:'/search_result', component:search_result}
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router

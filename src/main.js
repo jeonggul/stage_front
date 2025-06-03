1607396e297232a28c928eee5c10c24f7da5bf32
ZIBAGO <cownsgur01@gmail.com>
2025-06-03 22:36:04 +0900

    라우팅 수정

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-/*import Detail_page from './views/detail_page.vue'*/
 
 createApp(App).use(router).mount('#app')
-
-/*app.use(router)
-
-app.mount('#app')*/
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import login from '@/views/login.vue'
 import signup from '@/views/signup.vue'
 import signup_details from '@/views/signup_details.vue'
 import search_result from '@/views/search_result.vue'
+import detail_page from '../views/detail_page.vue'
 
 const routes = [
-    {path:'/', redirect: '/login'},
-    {path:'/login', component: login},
-    {path: '/signup', component: signup},
-    {path: '/signup_details', component: signup_details},
-    {path:'/search_result', component:search_result}
+  {path: '/', redirect: '/login'},
+  {path: '/login', component: login},
+  {path: '/signup', component: signup},
+  {path: '/signup_details', component: signup_details},
+  {path: '/search_result', component: search_result},
+  {path: '/detail_page',  component: detail_page}
 ]
 
 const router = createRouter({
